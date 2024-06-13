7d91a3703436c53caa16a14d94b383aec6d1bc25
hsoaresbianca <biasoares.bianca@gmail.com>
2024-06-13 15:10:20 -0300

    updating app

--- app.ts
+++ app.ts
@@ -1,27 +1,23 @@
-
-import { PeopleAccount } from './class/PeopleAccount'
 import { CompanyAccount } from './class/CompanyAccount'
+import { PeopleAccount } from './class/PeopleAccount'
 import { PlusTenAccount } from './class/PlusTenAccount'
 
 
-// EXECUTANDO PEOPLE ACCOUNT:
-// const peopleAccount: PeopleAccount = new PeopleAccount(123, 'Bia', 1)
-// peopleAccount.getBalance()
-// peopleAccount.deposit(500)
-// peopleAccount.withdraw(600)
-// peopleAccount.setName('Bianca Soares')
-
-
 // EXECUTANDO COMPANY ACCOUNT:
-// const companyAccount: CompanyAccount = new CompanyAccount('Dio', 2)
-// companyAccount.deposit(100)
-// companyAccount.getBalance()
-// companyAccount.getLoan(100)
-// companyAccount.getBalance()
+const companyAccount: CompanyAccount = new CompanyAccount('Dio', 2)
+console.log(companyAccount)
+companyAccount.getLoan(1500)
 
 
-// EXECUTANDO PLUS TEN ACCOUNT:
+// EXECUTANDO PEOPLE ACCOUNT:
+const peopleAccount: PeopleAccount = new PeopleAccount(123, 'Bia', 1)
+console.log(peopleAccount)
+peopleAccount.deposit(100)
+peopleAccount.withdraw(200)
+peopleAccount.withdraw(50)
+
+
+// // EXECUTANDO PLUS TEN ACCOUNT:
 const plusTenAccount: PlusTenAccount = new PlusTenAccount('Mary', 3)
-plusTenAccount.getBalance()
+console.log(plusTenAccount)
 plusTenAccount.deposit(100)
-plusTenAccount.getBalance()
