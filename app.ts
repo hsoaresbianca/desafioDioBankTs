d2f0cafb5726beea44b6c0c5e3405419f039e01d
hsoaresbianca <biasoares.bianca@gmail.com>
2024-06-13 12:02:57 -0300

    updating app

--- app.ts
+++ app.ts
@@ -3,6 +3,8 @@ import { PeopleAccount } from './class/PeopleAccount'
 import { CompanyAccount } from './class/CompanyAccount'
 import { PlusTenAccount } from './class/PlusTenAccount'
 
+
+// EXECUTANDO PEOPLE ACCOUNT:
 // const peopleAccount: PeopleAccount = new PeopleAccount(123, 'Bia', 1)
 // peopleAccount.getBalance()
 // peopleAccount.deposit(500)
@@ -10,12 +12,15 @@ import { PlusTenAccount } from './class/PlusTenAccount'
 // peopleAccount.setName('Bianca Soares')
 
 
+// EXECUTANDO COMPANY ACCOUNT:
 // const companyAccount: CompanyAccount = new CompanyAccount('Dio', 2)
 // companyAccount.deposit(100)
 // companyAccount.getBalance()
 // companyAccount.getLoan(100)
 // companyAccount.getBalance()
 
+
+// EXECUTANDO PLUS TEN ACCOUNT:
 const plusTenAccount: PlusTenAccount = new PlusTenAccount('Mary', 27)
 plusTenAccount.getBalance()
 plusTenAccount.deposit(100)
